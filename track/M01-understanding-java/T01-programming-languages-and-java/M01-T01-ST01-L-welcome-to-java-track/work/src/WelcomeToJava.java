71ec5fcfb9a7d6ad44ca56c5bc264592de4a9719
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-07-30 20:30:09 +0530

    One more change

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -7,5 +7,6 @@ public class WelcomeToJava {
         System.out.println("Now i ready how to express that logic using java.");
         System.out.println("Now i know how to use git and github.");
         System.out.println("Now i know how to push my code to github");
+        System.out.println("I am ready to learn formore");
     }
 }
