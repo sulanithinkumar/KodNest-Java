754692a8674f5685babbba4f8fde8c15b16f3616
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-07-30 12:45:14 +0530

    day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to the Java track.");
+        System.out.println("I already to Know how to build programming logic.");
+        System.out.println("Now i ready how to express that logic using java.");
     }
 }
