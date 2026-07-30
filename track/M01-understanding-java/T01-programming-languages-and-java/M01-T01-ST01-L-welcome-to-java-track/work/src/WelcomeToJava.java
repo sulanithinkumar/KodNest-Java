dd674b44edb1d902b9645498911d8e3e13c89cab
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-07-30 13:02:57 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,5 +5,6 @@ public class WelcomeToJava {
         System.out.println("Welcome to the Java track.");
         System.out.println("I already to Know how to build programming logic.");
         System.out.println("Now i ready how to express that logic using java.");
+        System.out.println("Now i know how to use git and github.");
     }
 }
