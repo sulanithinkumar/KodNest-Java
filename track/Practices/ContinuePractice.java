2f1a32cc5dbe2c2affe03ecfc0068c4198bc1335
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-08-06 12:28:39 +0530

    Continue Practice

---
+++ track/Practices/ContinuePractice.java
@@ -0,0 +1,14 @@
+
+public class ContinuePractice {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 7) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
