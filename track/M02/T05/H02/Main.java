3161c5cdaf1260e9db5799f248e1cb384e587a36
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-08-12 01:20:00 +0530

    M02 T05 H02 Main.java pushing

---
+++ track/M02/T05/H02/Main.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+class Employee {
+
+    String name;
+    double salary;
+
+    void setDetails(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Employee Name: " + this.name);
+        System.out.println("Salary: " + this.salary);
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter full name: ");
+        String fullName = scanner.nextLine();
+
+        System.out.println("Enter salary: ");
+        double salary = scanner.nextDouble();
+
+        Employee emp = new Employee();
+
+        emp.setDetails(fullName, salary);
+        emp.displayDetails();
+    }
+}
