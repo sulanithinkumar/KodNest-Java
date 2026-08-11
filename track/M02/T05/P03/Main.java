423d6f85c2baa3b524de957febd09eddfb71509c
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-08-12 01:02:16 +0530

    M02 T05 P03 Main.java Pushing

---
+++ track/M02/T05/P03/Main.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+class Student {
+
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+
+    }
+
+    void displayName() {
+        System.out.println("Student Name: " + this.name);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Name: ");
+        String name = scanner.nextLine();
+
+        Student s = new Student();
+        s.setName(name);
+        s.displayName();
+
+    }
+
+}
