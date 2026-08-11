f1f8a066c887d84487d329889ec2e3e5b894e6ba
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-08-12 00:23:04 +0530

    M02 T05 P01

---
+++ track/M02/T05/P01/Main.java
@@ -0,0 +1,17 @@
+
+class Robot {
+
+    void speak() {
+        System.out.println("Beep beep! Java is my superpower!");
+
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Robot r = new Robot();
+        r.speak();
+    }
+
+}
