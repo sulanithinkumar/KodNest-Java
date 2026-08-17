6519d9e76a0ed027edfff377b493ebbea6188729
Sula Nithin kumar <sulanithinkumar@gmail.com>
2026-08-17 12:29:57 +0530

    Array practices

---
+++ track/Practices/ArrayPractices/Prgm3.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Prgm3 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter size of array:");
+        int a[] = new int[scan.nextInt()];
+
+        System.out.println("Enter the array values: ");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+
+            a[i] = scan.nextInt();
+        }
+
+        System.out.println("Array values are: ");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+
+}
---
+++ track/Practices/ArrayPractices/prgm2.java
@@ -0,0 +1,14 @@
+
+public class prgm2 {
+
+    public static void main(String[] args) {
+        int a[] = {11, 22, 33, 44, 55};
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+    }
+}
